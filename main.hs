77971e33cb7910b0d23492cbe134e5fc58c03e6d
unknown <alexandreamk1@gmail.com>
2019-11-02 11:20:36 -0200

    .

--- main.hs
+++ main.hs
@@ -1,9 +1,18 @@
-import Matrix
-import Reversi
-import ReversiIA
+module Main where
+
+    {-Data import-}
+    import Data.Char
+    import Data.Map (Map, (!))
+    import qualified Data.Map as Map
+    {-This project imports-}
+    import Generic
+    import Reversi
 
 -- Compiacao: ghc --make main.hs
 -- :set +m   Serve para habilitar multiplas linhas no interpretador
+
+sizemap = 8
+
 main = do 
 {
     let a = line (create 4 4 (0, ' ')) 3 
